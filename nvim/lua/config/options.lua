e423a679771958859bcea9774f09cca38a8a8d14
Martynas Šapalas <8864073+maaarsas@users.noreply.github.com>
2026-09-01 13:12:33 +0300

    Add nvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt
+
+opt.cursorline = true -- Enable highlighting of the current line
+opt.list = true -- Show some invisible characters (tabs...
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true             -- Show line numbers
+opt.relativenumber = false    -- Absolute line numbers
+opt.termguicolors = true -- True color support
